1bb9645058a779923feb9a402e20627b3d6a523d
ajnobleza24-zzzz <ajnobleza24@gmail.com>
2026-05-04 21:50:55 +0800

    Update dashboard.php

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -10,9 +10,41 @@ session_start();
     <link rel="stylesheet" href="../css/style.css">
 </head>
 <body>
+    <h1>Admin Dashboard</h1>
+<p>Welcome, Admin</p>
+
+<div class="container">
+
+    <div class="card">
+        <h2>Reports</h2>
+
+        <table>
+            <tr> // Report Management 
+                <th>Problem</th>
+                <th>Location</th>
+                <th>Status</th>
+                <th>Action</th>
+            </tr>
+
+          
+            <tr>
+                <td>No data yet</td>  // Problem Column
+                <td>No data yet</td>  // Location Column
+                <td><span class="status pending">Pending</span></td>   // Status Tracking System 
+                <td><button>Update</button></td>
+            </tr>
+
+        </table>
+    </div>
+
+</div>
+
 
 <h1>Admin Dashboard</h1>
 <p>Welcome, Admin</p>
+
+
+    
 
 </body>
 </html>
