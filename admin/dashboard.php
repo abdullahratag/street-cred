450337fac806cbb5ce9a3b4bbc5ae49bd8260727
abdullahratag <co240280@adzu.edu.ph>
2026-05-04 14:25:00 +0800

    basic admin dashboard structure

---
+++ admin/dashboard.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+// TODO: Add admin role check later
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Admin Dashboard - Street Cred</title>
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+<body>
+
+<h1>Admin Dashboard</h1>
+<p>Welcome, Admin</p>
+
+</body>
+</html>
