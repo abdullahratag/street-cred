e831970af08b0198a6eeead3ca7e4dfeb27f5f94
ajnobleza24-zzzz <ajnobleza24@gmail.com>
2026-05-20 01:17:47 +0800

    Add files via upload

---
+++ login-page/script.js
@@ -0,0 +1,12 @@
+
+function login() {
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+
+    if (username == "" || password == "") {
+        alert("Please fill in all fields.");
+    } else {
+        alert("Login successful (demo only). Welcome to StreetCred!");
+    }
+}
+
